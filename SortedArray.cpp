d1cb50cd77307a934969c0bc76d5d34e0871cfdd
wavpro <31097540+wavpro@users.noreply.github.com>
2022-10-23 10:04:08 +0200

    Formatted SortedArray.cpp
    Improved readability

--- SortedArray.cpp
+++ SortedArray.cpp
@@ -1,41 +1,38 @@
 #include<iostream>
+
 #include<conio.h>
+
 using namespace std;
-class sorted
-{
-      int a[1000];
-      public:
-             void getdata()
-             {    
-                  int n;
-                  cout<<"Enter the number of terms: ";
-                  cin>>n;
-                  cout<<"Enter the terms: ";
-                  for(int i=0;i<n;i++)
-                  cin>>a[i];
-                  cout<<"unsorted array:";
-                  for(int i=0;i<n;i++)
-                  cout<<a[i]<<",";
-             for(int i=0;i<n;i++)
-            {
-             for(int j=i+1;j<n;j++)
-             {
-              if(a[i]<=a[j])
-              { int temp;
-                            temp=a[i];
-                            a[i]=a[j];
-                            a[j]=temp;
-              }
-           }
-     }
-            cout<<endl<<"sorted array: ";
-for(int i=0;i<n;i++)
-cout<<a[i]<<",";  
-}      
+class sorted {
+  int a[1000];
+  public:
+    void getdata() {
+      int n;
+      cout << "Enter the number of terms: ";
+      cin >> n;
+      cout << "Enter the terms: ";
+      for (int i = 0; i < n; i++)
+        cin >> a[i];
+      cout << "unsorted array:";
+      for (int i = 0; i < n; i++)
+        cout << a[i] << ",";
+      for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+          if (a[i] <= a[j]) {
+            int temp;
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+          }
+        }
+      }
+      cout << endl << "sorted array: ";
+      for (int i = 0; i < n; i++)
+        cout << a[i] << ",";
+    }
 };
-int main()
-{
-        sorted b;
-        b.getdata();
-retun 0;
+int main() {
+  sorted b;
+  b.getdata();
+  retun 0;
 }
