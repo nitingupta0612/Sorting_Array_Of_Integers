750cfcc24053a0b907987a20ab26d95ea7bf97cf
Atharv Phadnis <athiphadnis@gmail.com>
2022-10-25 11:16:29 +0530

    Removed Conio.h and Fixed typo

--- SortedArray.cpp
+++ SortedArray.cpp
@@ -1,38 +1,44 @@
-#include<iostream>
-
-#include<conio.h>
-
+#include <iostream>
 using namespace std;
-class sorted {
+class sorted
+{
   int a[1000];
-  public:
-    void getdata() {
-      int n;
-      cout << "Enter the number of terms: ";
-      cin >> n;
-      cout << "Enter the terms: ";
-      for (int i = 0; i < n; i++)
-        cin >> a[i];
-      cout << "unsorted array:";
-      for (int i = 0; i < n; i++)
-        cout << a[i] << ",";
-      for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-          if (a[i] <= a[j]) {
-            int temp;
-            temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-          }
+
+public:
+  void getdata()
+  {
+    int n;
+    cout << "Enter the number of terms: ";
+    cin >> n;
+    cout << "Enter the terms: ";
+    for (int i = 0; i < n; i++)
+      cin >> a[i];
+    cout << "unsorted array:";
+    for (int i = 0; i < n; i++)
+      cout << a[i] << ",";
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = i + 1; j < n; j++)
+      {
+        if (a[i] <= a[j])
+        {
+          int temp;
+          temp = a[i];
+          a[i] = a[j];
+          a[j] = temp;
         }
       }
-      cout << endl << "sorted array: ";
-      for (int i = 0; i < n; i++)
-        cout << a[i] << ",";
     }
+    cout << endl
+         << "sorted array: ";
+    for (int i = 0; i < n; i++)
+      cout << a[i] << ",";
+  }
 };
-int main() {
+
+int main()
+{
   sorted b;
   b.getdata();
-  retun 0;
+  return 0;
 }
